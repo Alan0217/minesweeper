3800feac0d68d8e53b3aaf15f5e3c4e54eaa9e1e
Alan0217 <Alan0217@users.noreply.github.com>
2024-05-11 04:35:12 +0000

    deploy: 5c4a0eae2e07510a1f20bdb4adfdeae80b976d73

---
+++ _next/static/fKuPsJGMsdhya44G9CT8R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/c01d9a3684ff5b07.css","static/chunks/pages/index-26b4539dc777e289.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
